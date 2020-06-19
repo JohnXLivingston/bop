2b18f115a6969126dc6ca35d679f721d1075cbdb
Jonathan Dollé <git@jonathandolle.fr>
2020-06-19 03:24:47 +0200

    New way to extract gettext. In typescript and templates sources.

---
+++ scripts/i18n/extract.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+console.log('Searching gettext in typescript sources...')
+
+const { GettextExtractor, JsExtractors } = require('gettext-extractor')
+
+let extractor = new GettextExtractor()
+
+extractor.createJsParser([
+  JsExtractors.callExpression('gettext', {
+    arguments: {
+      text: 0
+    }
+  })
+]).parseFilesGlob('./src/**/*.@(ts|js|tsx|jsx)')
+
+
+extractor.savePotFile('./src/locale/templates/LC_MESSAGES/messages.pot');
+
+extractor.printStats();
+
+console.log('Continuing to nunjucks templates with jsxgettext...')
+
+exec = require('child_process').exec
+const jsxgettextCmd = './node_modules/.bin/jsxgettext -j -L jinja \
+   --output-dir=src/locale/templates/LC_MESSAGES \
+   --from-code=utf-8 \
+   --output=messages.pot \
+  `find ./src -name \'*.html\' -o -name \'*.njk\' | grep -v node_modules | grep -v "\.git"`;'
+exec(jsxgettextCmd), function (err, stdout, stderr) {
+  if (err) {
+    console.error(stdout);
+    console.error(stderr);
+    throw "Error."
+  }
+  console.log('done');
+
+}
+
+
+// #!/bin/bash
+
+// # -e: exit on error
+// set -e
+
+// export NODE_ENV="production"
+
+// npm run clean;
+// npm run build;
+
+// ./node_modules/.bin/jsxgettext -L javascript \
+//   --output-dir=src/locale/templates/LC_MESSAGES \
+//   --from-code=utf-8 \
+//   --output=messages.pot \
+//   `find ./dist -name '*.js' | grep -v node_modules | grep -v "\.git"`;
+
+// ./node_modules/.bin/jsxgettext -j -L jinja \
+//   --output-dir=src/locale/templates/LC_MESSAGES \
+//   --from-code=utf-8 \
+//   --output=messages.pot \
+//   `find ./dist -name '*.html' -o -name '*.njk' | grep -v node_modules | grep -v "\.git"`;
+
