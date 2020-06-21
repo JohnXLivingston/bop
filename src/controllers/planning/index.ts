4410cf54fea0dcb28e2bd4ac4a23d2d11ebb23d7
Jonathan Dollé <git@jonathandolle.fr>
2020-06-21 18:53:29 +0200

    WIP.

--- src/controllers/planning/index.ts
+++ src/controllers/planning/index.ts
@@ -4,8 +4,29 @@ import { authenticatedOrLogin, i18nChangeLocale } from '../../middlewares'
 const planningRouter = express.Router()
 
 planningRouter.get('', i18nChangeLocale, authenticatedOrLogin, (req, res) => {
+  const nodes: any[] = []
+  nodes.push({
+    name: 'Resource 1',
+    childs: [
+      {
+        name: 'Sub resource 1.a',
+        childs: [{
+          name: 'Sub sub resource 1.a.1'
+        }]
+      }
+    ]
+  })
+  for (let i = 2; i <= 99; i++) {
+    nodes.push({
+      name: 'Resource ' + i
+    })
+  }
   res.render('planning.njk', {
-    currentPage: 'planning'
+    currentPage: 'planning',
+    nodes: nodes,
+    planningProperties: {
+      nbWeeks: 9
+    }
   })
 })
 
