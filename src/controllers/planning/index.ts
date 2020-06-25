d233e9d30c195d56d45332853d78aa42cff86eca
Jonathan Dollé <git@jonathandolle.fr>
2020-06-25 06:23:31 +0200

    WIP on styles.

--- src/controllers/planning/index.ts
+++ src/controllers/planning/index.ts
@@ -8,15 +8,19 @@ planningRouter.get('', i18nChangeLocale, authenticatedOrLogin, (req, res) => {
   nodes.push({
     name: 'Resource 1',
     data: [
-      { left: 0, width: 90, label: '10 h/j', color: 'red' },
-      { left: 135, width: 45, label: '8 h/j', color: 'green' }
+      { left: 0, width: 90, label: '10 h/j', color: 'red', type: 'summary' },
+      { left: 135, width: 45, label: '8 h/j', color: 'green', type: 'summary' }
     ],
     childs: [
       {
         name: 'Sub resource 1.a',
         childs: [{
           name: 'Sub sub resource 1.a.1'
-        }]
+        }],
+        data: [
+          { left: 45, width: 180, label: '8h/j', color: '#79c5ee', type: 'task' },
+          { left: 360, width: 45, label: '8h/j', color: '#79c5ee', type: 'task' }
+        ]
       }
     ]
   })
