7a6b2c409e283028357123a60014636270328991
Jonathan Dollé <git@jonathandolle.fr>
2020-06-27 12:26:48 +0200

    Planning colors.

--- src/controllers/planning/index.ts
+++ src/controllers/planning/index.ts
@@ -9,14 +9,14 @@ planningRouter.get('', i18nChangeLocale, authenticatedOrLogin, (req, res) => {
     name: 'Resource 1',
     type: 'summary',
     data: [
-      { left: 0, width: 90, label: '10 h/j', color: 'red' },
-      { left: 135, width: 45, label: '8 h/j', color: 'green' }
+      { left: 0, width: 90, label: '10 h/j', color: 'overloaded' },
+      { left: 135, width: 45, label: '8 h/j', color: 'full' }
     ],
     childs: [
       {
         name: 'Sub resource 1.a',
         type: 'task',
-        color: '#79c5ee',
+        color: '1',
         stubGroups: [
           {
             left: 45,
@@ -29,7 +29,7 @@ planningRouter.get('', i18nChangeLocale, authenticatedOrLogin, (req, res) => {
         childs: [{
           name: 'Sub sub resource 1.a.1',
           type: 'task',
-          color: '#79c5ee',
+          color: '2',
           stubGroups: [
             {
               left: 315,
