6b53859c76927245b1ed2d911f2408992e79c39b
Jonathan Dollé <git@jonathandolle.fr>
2020-07-02 02:24:54 +0200

    New way to load webpackManifest.

--- src/helpers/config/webpack.ts
+++ src/helpers/config/webpack.ts
@@ -5,4 +5,91 @@ import { isTest } from './env'
 
 const filename: string = path.join(__dirname, '../../assets.json')
 const raw: string = isTest ? '{}' : fs.readFileSync(filename, 'utf-8')
-export const webpackManifest = JSON.parse(raw)
+
+interface WebpackAsset {
+  css?: string,
+  js?: string,
+  text?: string
+}
+
+type WebpackAssets = {[key: string]: WebpackAsset}
+
+const assets: WebpackAssets = JSON.parse(raw)
+let headerManifestScriptTxt: string
+const cssFiles: {[key: string]: string[]} = {}
+const jsFiles: {[key: string]: string[]} = {}
+for (const key in assets) {
+  const asset = assets[key]
+  if (key === '') {
+    // - '' : here are some images and other stuff
+    continue
+  }
+  if (key === 'manifest') {
+    // - 'manifest' : the webpack manifest
+    if (asset.text) {
+      headerManifestScriptTxt = asset.text
+    }
+    continue
+  }
+  let parts: string[]
+  const m = key.match(/^[a-z]+-([^-]*)-.*$/)
+  if (m) {
+    // - cacheGroupKey-chunk1(~chunk2~... )?-modulefilename
+    // (see webpack.config.js)
+    parts = m[1].split('~').filter(s => s !== '')
+  } else {
+    // - chunck~.
+    // - vendors~chunk1~(chunk2=~)*.
+    parts = key.split('.')[0].split('~')
+    parts = parts.filter(s => s !== '' && s !== 'vendors')
+  }
+  for (let i = 0; i < parts.length; i++) {
+    const part = parts[i]
+    if (asset.js) {
+      if (!jsFiles[part]) { jsFiles[part] = [] }
+      jsFiles[part].push(asset.js)
+    }
+    if (asset.css) {
+      if (!cssFiles[part]) { cssFiles[part] = [] }
+      cssFiles[part].push(asset.css)
+    }
+  }
+}
+
+// FIXME: cssFiles should be sorted by correct order
+// FIXME: jssFiles should be sorted by dependencies
+
+// WebpackManifest will be used in templates.
+// This means that attributes like 'private' or
+// 'readonly' can be ignored.
+// To avoid malicious code to accidently modify
+// scripts and inject code, data are not stored
+// on the object, but in variables.
+class WebpackManifest {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  headerScriptTxt (name: string) {
+    // This code will be used in templates.
+    // To avoid some code injection, we must always return
+    // a clone.
+    return headerManifestScriptTxt
+  }
+
+  stylesheetUrls (name: string) {
+    if (!cssFiles[name]) {
+      return []
+    }
+    // clone array!
+    return cssFiles[name].slice()
+  }
+
+  scriptUrls (name: string, locale: string) {
+    const r = jsFiles[name] ? jsFiles[name].slice() : []
+    if (/^\w+_\w+$/.test(locale)) {
+      // FIXME: i18n files should pass through webpack, to minimize and add an hash in url.
+      r.unshift(`i18n/${locale}/messages.js`)
+    }
+    return r
+  }
+}
+
+export const webpackManifest = new WebpackManifest()
