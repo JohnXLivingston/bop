149beb2f86cc511cf29b93ed92ec0db5540f9d34
Jonathan Dollé <git@jonathandolle.fr>
2020-07-03 20:03:27 +0200

    jQuery.bopData... methods.

--- src/public/js/jquery/utils.ts
+++ src/public/js/jquery/utils.ts
@@ -1,5 +1,20 @@
+import getLogger from '../utils/logger'
+
+const logger = getLogger('jquery/utils')
+
+interface BopDataMethod<T> {
+  (this: JQuery, name: string): T
+  (this: JQuery, name: string, value: T): JQuery
+}
+
 declare global {
   interface JQuery {
+    bopDataString: BopDataMethod<string>
+    bopDataBoolean : BopDataMethod<boolean>
+    bopDataObject: BopDataMethod<object>
+    bopDataArray: BopDataMethod<Array<any>>
+    bopDataAny: BopDataMethod<any>
+
     /**
      * Like jQuery.find, but can also find the element itself.
      * @param el The selector, or the JQuery element where to search.
@@ -17,14 +32,110 @@ declare global {
   }
 }
 
-/**
- * Like jQuery.find, but can also find the element itself.
- * @param el The selector, or the JQuery element where to search.
- * @param selector the selector to search.
- */
+const bopDataString: BopDataMethod<string> = function (this: JQuery, name: string, value?: string): any {
+  const el = $(this)
+  if (arguments.length <= 1) {
+    return el.attr(name) || ''
+  }
+  return el.attr(name, '' + value)
+}
+
+const bopDataBoolean: BopDataMethod<boolean> = function (this: JQuery, name: string, value?: boolean): any {
+  const el = $(this)
+  if (arguments.length <= 1) {
+    const s = el.attr(name)
+    return s && s !== '0' && s !== 'false'
+  }
+  return el.attr(name, value ? 1 : null)
+}
+
+const bopDataObject: BopDataMethod<object> = function (this: JQuery, name: string, value?: object): any {
+  if (arguments.length <= 1) {
+    const r = (bopDataAny as (this: JQuery, name: string) => any).call(this, name)
+    if (r === null) {
+      // This case is not an error.
+      return {}
+    }
+    if (typeof r !== 'object') {
+      logger.error('bopDataObject: Attribute ' + name + ' is not an object, but a ' + (typeof r), $(this))
+      return {}
+    }
+    if (Array.isArray(r)) {
+      logger.error('bopDataObject: Attribute ' + name + ' is an array. Not valid.', $(this))
+      return {}
+    }
+    return r
+  }
+
+  if (typeof value !== 'object') {
+    logger.error('bopDataObject: the value is not an object. This is not valid.', value)
+    value = {}
+  }
+  if (Array.isArray(value)) {
+    logger.error('bopDataObject: the value is an array. This is not valid.', value)
+    value = {}
+  }
+  return bopDataAny.call(this, name, value)
+}
+
+const bopDataArray: BopDataMethod<Array<any>> = function (this: JQuery, name: string, value?: Array<any>): any {
+  if (arguments.length <= 1) {
+    const r = (bopDataAny as (this: JQuery, name: string) => any).call(this, name)
+    if (r === null || r === undefined) {
+      // This case is not an error.
+      return []
+    }
+    if (typeof r !== 'object') {
+      logger.error('bopDataArray: Attribute ' + name + ' is not an object, but a ' + (typeof r), $(this))
+      return []
+    }
+    if (!Array.isArray(r)) {
+      logger.error('bopDataObject: Attribute ' + name + ' is not an array. Not valid.', $(this))
+      return []
+    }
+    return r
+  }
+
+  if (typeof value !== 'object') {
+    logger.error('bopDataArray: the value is not an object. This is not valid.', value)
+    value = []
+  }
+  if (!Array.isArray(value)) {
+    logger.error('bopDataArray: the value is not an array. This is not valid.', value)
+    value = []
+  }
+  return bopDataAny.call(this, name, value)
+}
+
+const bopDataAny: BopDataMethod<any> = function (this: JQuery, name: string, value?: any): any {
+  const el = $(this)
+  if (arguments.length <= 1) {
+    const s = el.attr(name)
+    if (s === undefined) {
+      return undefined
+    }
+    let o
+    try {
+      o = JSON.parse(s)
+    } catch (e) {
+      logger.error('bopDataAny: Can\'t parse JSON in attribute ' + name, el)
+      return {}
+    }
+    return o
+  }
+
+  return el.attr(name, JSON.stringify(value))
+}
+
 jQuery.fn.extend({
-  bopFindSelf: function bopFindSelf (selector: string): JQuery {
-    const el = $(this as JQuery)
+  bopDataString,
+  bopDataBoolean,
+  bopDataObject,
+  bopDataArray,
+  bopDataAny,
+
+  bopFindSelf: function bopFindSelf (this: JQuery, selector: string): JQuery {
+    const el = $(this)
     return el.filter(selector).add(el.find(selector))
   }
 })
