f7489f105c01de33e29264f7db6608e12a1d898a
Jonathan Dollé <git@jonathandolle.fr>
2020-07-19 12:17:44 +0200

    Wheelmenu WIP.

--- src/public/js/widgets/wheelmenu.ts
+++ src/public/js/widgets/wheelmenu.ts
@@ -41,21 +41,42 @@ $.widget('bop.bopWheelmenu', {
       for (let i = 0; i < items.length; i++) {
         const item = $(items[i])
         content.append(item)
-        const alpha = i * 2 * Math.PI / items.length
-        const x = Math.round(this.options.radius * Math.cos(alpha))
-        const y = Math.round(this.options.radius * Math.sin(alpha))
-        item.css('left', x + 'px')
-        item.css('top', y + 'px')
       }
       const overlay = $('<div class="widget-wheelmenu-overlay"></div>')
+
+      let zindex: number | undefined
+      $('.widget-wheelmenu-overlay').each((i, html) => {
+        const o = $(html)
+        const z = o.css('z-index')
+        if (zindex === undefined || +z > zindex) { zindex = +z }
+      })
+      if (zindex) {
+        overlay.css('z-index', zindex + 2)
+        content.css('z-index', zindex + 3)
+      }
+
+      // (centerX, centerY) is the position of the event (cartesian coordinates)
+      const centerX = ev.pageX || 0
+      const centerY = ev.pageY || 0
+      content.css('left', centerX + 'px')
+      content.css('top', centerY + 'px')
 
       $('body').append(content)
       $('body').append(overlay)
 
-      content.position({
-        my: 'center bottom',
-        at: 'left+' + ev.pageX + ' top+' + ev.pageY,
-        of: window
+      content.find('li').each((i, html) => {
+        const item = $(html)
+        const width = item.outerWidth() || 0
+        const height = item.outerHeight() || 0
+        const alpha = (i * 2 * Math.PI / items.length) - (Math.PI / 2)
+        // (cX, cY) is the position of the item's center (cartesian coordinates)
+        const cX = this.options.radius * Math.cos(alpha)
+        const cY = this.options.radius * Math.sin(alpha)
+        // (x, y) is the cordinates for the top left corner
+        const x = Math.round(cX - (width / 2))
+        const y = Math.round(cY - (height / 2))
+        item.css('left', x + 'px')
+        item.css('top', y + 'px')
       })
 
       const close = () => {
