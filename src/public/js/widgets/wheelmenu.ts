4ac0f7ddc1247c9dfe76dacd5bfbe42609d0135d
Jonathan Dollé <git@jonathandolle.fr>
2020-07-21 03:23:34 +0200

    Some event handling.

--- src/public/js/widgets/wheelmenu.ts
+++ src/public/js/widgets/wheelmenu.ts
@@ -24,6 +24,15 @@ const bopWheelmenuDefaultOptions: BopWheelmenuOptions = {
   radius: 100
 }
 
+function maxWheelmenuOverlayZIndex (): number | undefined {
+  let zindex: number | undefined
+  const lastOverlay = $('.widget-wheelmenu-overlay:last')
+  if (lastOverlay.length) {
+    return +lastOverlay.css('z-index')
+  }
+  return zindex
+}
+
 $.widget('bop.bopWheelmenu', {
   options: bopWheelmenuDefaultOptions,
   _create: function () {
@@ -44,13 +53,10 @@ $.widget('bop.bopWheelmenu', {
       }
       const overlay = $('<div class="widget-wheelmenu-overlay"></div>')
 
-      let zindex: number | undefined
-      $('.widget-wheelmenu-overlay').each((i, html) => {
-        const o = $(html)
-        const z = o.css('z-index')
-        if (zindex === undefined || +z > zindex) { zindex = +z }
-      })
+      const zindex = maxWheelmenuOverlayZIndex()
+      let namespace: string = 'wheelmenu'
       if (zindex) {
+        namespace += zindex
         overlay.css('z-index', zindex + 2)
         content.css('z-index', zindex + 3)
       }
@@ -70,6 +76,11 @@ $.widget('bop.bopWheelmenu', {
         // First, we center the item.
         const width = item.outerWidth() || 0
         const height = item.outerHeight() || 0
+        if (item.find('a').length) {
+          item.find('a').attr('tabindex', i + 1)
+        } else {
+          item.attr('tabindex', i + 1)
+        }
         item.css('left', 0 - (width / 2))
         item.css('top', 0 - (height / 2))
 
@@ -85,10 +96,30 @@ $.widget('bop.bopWheelmenu', {
       const close = () => {
         content.remove()
         overlay.remove()
+        $('body').off('.' + namespace)
       }
       overlay.on('click', close)
+      overlay.on('contextmenu', () => false)
+      $('body').on('keyup.' + namespace, (ev) => {
+        // I have to check if the top level wheelmenu is me.
+        if (+overlay.css('z-index') !== maxWheelmenuOverlayZIndex()) {
+          return true
+        }
+        if (ev.key === 'Escape') {
+          close()
+          ev.preventDefault()
+          ev.stopImmediatePropagation()
+          return false
+        }
+        return true
+      })
 
       parseWidgets(content)
+      if (content.find('li:first a').length) {
+        content.find('li:first a').focus()
+      } else {
+        content.find('li:first').focus()
+      }
     }
     if (this.options.onClick) {
       widget.on('click', handler)
