21a1be2df5dc4e1f3eeb36d90ec9d6035fe18856
Jonathan Dollé <git@jonathandolle.fr>
2020-08-17 15:14:19 +0200

    Resource.

--- src/shared/models/bop-object.model.ts
+++ src/shared/models/bop-object.model.ts
@@ -1,5 +1,6 @@
 import { Project } from './project'
 import { Task } from './task'
 import { User } from './user'
+import { Resource } from './resource'
 
-export type BopObject = Project | Task | User
+export type BopObject = Project | Task | User | Resource
